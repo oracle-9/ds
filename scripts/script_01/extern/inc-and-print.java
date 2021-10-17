eb171b9f2cc0713b309f3d33595cbcc16221e3ef
oracle <oracle-main@pm.me>
2021-10-17 15:00:52 +0100

    Format script 1 external resources.

--- scripts/script_01/extern/inc-and-print.java
+++ scripts/script_01/extern/inc-and-print.java
@@ -1,8 +1,7 @@
 class Increment implements Runnable {
-  public void run() {
-    final long I=100;
-
-    for (long i = 0; i < I; i++)
-      System.out.println(i);
-  }
+    public void run() {
+        final long I = 100;
+        for (long i = 0; i < I; i++)
+            System.out.println(i);
+    }
 }
