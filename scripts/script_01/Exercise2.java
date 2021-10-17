118890b75f8bfff50969fa17c0cc884b3cb5d85e
oracle <oracle-main@pm.me>
2021-10-17 23:11:36 +0100

    Add script_01/Exercise2.java

---
+++ scripts/script_01/Exercise2.java
@@ -0,0 +1,44 @@
+import extern.Bank;
+
+import java.util.ArrayList;
+
+import static java.lang.System.out;
+
+public final class Exercise2 {
+    public static void main(String... args) {
+        final var N_THREADS = 10;
+        final var N_DEPOSITS = 1000;
+        final var DEPOSIT_VALUE = 100;
+        final var EXPECTED_SAVINGS = N_THREADS * N_DEPOSITS * DEPOSIT_VALUE;
+
+        final var savings = Bank.Account.withBalance(0);
+
+        final var pool = new ArrayList<Thread>();
+        pool.ensureCapacity(N_THREADS);
+
+        for (var i = 0; i < 10; ++i) {
+            final var thread = new Thread(() -> {
+                for (var j = 0; j < N_DEPOSITS; ++j) {
+                    savings.deposit(DEPOSIT_VALUE);
+                }
+            });
+            thread.start();
+            pool.add(thread);
+        }
+
+        for (final var thread : pool) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+
+        out.print("""
+            Expected savings: %d
+            Actual savings:   %d
+            """.formatted(EXPECTED_SAVINGS, savings.balance())
+        );
+    }
+}
