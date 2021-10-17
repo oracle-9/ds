69a8fb19ab260f5dae9823a134d9ae0dbaa724e9
oracle <oracle-main@pm.me>
2021-10-17 16:17:21 +0100

    Add script 1 exercise 1.

---
+++ scripts/script_01/Exercise1.java
@@ -0,0 +1,31 @@
+import extern.Increment;
+
+import java.util.ArrayList;
+
+import static java.lang.System.out;
+
+public final class Exercise1 {
+    public static void main(String... args) {
+        final var N_THREADS = 10;
+        final var payload = new Increment();
+
+        final var pool = new ArrayList<Thread>();
+        pool.ensureCapacity(N_THREADS);
+
+        for (var i = 0; i < N_THREADS; ++i) {
+            final var thread = new Thread(payload);
+            thread.start();
+            pool.add(thread);
+        }
+
+        for (final var thread : pool) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        out.println("end");
+    }
+}
