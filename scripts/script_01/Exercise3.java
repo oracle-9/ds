8f48bacb39c29c30971d9b725938585485d13ec1
oracle <oracle-main@pm.me>
2021-10-18 01:20:53 +0100

    Add script_01/Exercise3.java.

---
+++ scripts/script_01/Exercise3.java
@@ -0,0 +1,49 @@
+import static java.lang.System.out;
+
+import java.util.ArrayList;
+import java.util.concurrent.locks.ReentrantLock;
+
+import extern.Bank;
+
+public final class Exercise3 {
+    public static void main(String... args) {
+        final var N_THREADS = 10;
+        final var N_DEPOSITS = 1000;
+        final var DEPOSIT_VALUE = 100;
+        final var EXPECTED_SAVINGS = N_THREADS * N_DEPOSITS * DEPOSIT_VALUE;
+
+        final var savings = Bank.Account.withBalance(0);
+        final var savingsLock = new ReentrantLock(); // alternatively, could be
+                                                     // a field of Bank.
+
+        final var pool = new ArrayList<Thread>();
+        pool.ensureCapacity(N_THREADS);
+
+        for (var i = 0; i < 10; ++i) {
+            final var thread = new Thread(() -> {
+                savingsLock.lock();
+                for (var j = 0; j < N_DEPOSITS; ++j) {
+                    savings.deposit(DEPOSIT_VALUE);
+                }
+                savingsLock.unlock();
+            });
+            thread.start();
+            pool.add(thread);
+        }
+
+        for (final var thread : pool) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+
+        out.print("""
+            Expected savings: %d
+            Actual savings:   %d
+            """.formatted(EXPECTED_SAVINGS, savings.balance())
+        );
+    }
+}
