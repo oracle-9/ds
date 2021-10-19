5c216e913623191245473ba62fbe1f98b06e6204
oracle <oracle-main@pm.me>
2021-10-19 03:31:08 +0100

    Solve script 2.

---
+++ scripts/script_02/extern/Bank.java
@@ -0,0 +1,241 @@
+package extern;
+
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.IntStream;
+
+public final class Bank {
+    private static final int DEFAULT_N_ACCOUNTS = 10;
+    private final Account[] accounts;
+    private final ThreadLocalRandom rng;
+    private final ReentrantLock lock;
+    private final Version version;
+
+    public enum Version {
+        ORIGINAL,
+        EXERCISE_1,
+        EXERCISE_2,
+        EXERCISE_3,
+    }
+
+    private Account getAccountOrThrow(final int accountId) {
+        if (accountId >= this.accounts.length) {
+            throw new RuntimeException("no such account");
+        }
+        return this.accounts[accountId];
+    }
+
+    private Bank(
+        final Account[] accounts,
+        final ThreadLocalRandom rng,
+        final ReentrantLock lock,
+        final Version version
+    ) {
+        this.accounts = accounts;
+        this.rng = rng;
+        this.lock = lock;
+        this.version = version;
+    }
+
+    public static final class Builder {
+        private Bank.Version version = Version.EXERCISE_3;
+        private int nAccounts = Bank.DEFAULT_N_ACCOUNTS;
+
+        public Bank.Builder withVersion(final Bank.Version version) {
+            this.version = version;
+            return this;
+        }
+
+        public Bank.Builder setUpAccounts(final int nAccounts) {
+            this.nAccounts = nAccounts;
+            return this;
+        }
+
+        public Bank build() {
+            final var accounts = new Account[this.nAccounts];
+            final var bankWideLock = this.version == Version.EXERCISE_1 ||
+                                     this.version == Version.EXERCISE_2
+                ? new ReentrantLock()
+                : null;
+
+            final var bank = new Bank(
+                accounts,
+                ThreadLocalRandom.current(),
+                bankWideLock,
+                this.version
+            );
+
+            switch (this.version) {
+            case EXERCISE_3 ->
+                Arrays.fill(accounts, bank.new Account(0, new ReentrantLock()));
+            default ->
+                Arrays.fill(accounts, bank.new Account(0, null));
+            }
+            return bank;
+        }
+    }
+
+    public int balanceOf(final int accountId) {
+        return switch (this.version) {
+        case ORIGINAL, EXERCISE_3 -> this.getAccountOrThrow(accountId).balance;
+        default -> {
+            this.lock.lock();
+            try {
+                yield this.getAccountOrThrow(accountId).balance;
+            } finally {
+                this.lock.unlock();
+            }
+            }
+        };
+    }
+
+    public void deposit(final int accountId, final int value) {
+        switch (this.version) {
+        case ORIGINAL, EXERCISE_3 ->
+            this.getAccountOrThrow(accountId).deposit(value);
+        default -> {
+            this.lock.lock();
+            try {
+                this.getAccountOrThrow(accountId).deposit(value);
+            } finally {
+                this.lock.unlock();
+            }
+        }
+        }
+    }
+
+    public void withdraw(final int accountId, final int value) {
+        switch (this.version) {
+        case ORIGINAL, EXERCISE_3 ->
+            this.getAccountOrThrow(accountId).withdraw(value);
+        default -> {
+            this.lock.lock();
+            try {
+                this.getAccountOrThrow(accountId).withdraw(value);
+            } finally {
+                this.lock.unlock();
+            }
+        }
+        }
+    }
+
+    public void transfer(
+        final int srcAccount,
+        final int destAccount,
+        final int value
+    ) {
+        switch (this.version) {
+        case EXERCISE_2 -> {
+            this.lock.lock();
+            Account src;
+            Account dest;
+
+            try {
+                src = this.getAccountOrThrow(srcAccount);
+                dest = this.getAccountOrThrow(destAccount);
+                src.withdraw(value);
+            } catch (final Exception e) {
+                this.lock.unlock();
+                throw e;
+            }
+
+            try {
+                dest.deposit(value);
+            } catch (final RuntimeException e) {
+                src.deposit(value); // should never throw.
+                throw e;
+            } finally {
+                this.lock.unlock();
+            }
+        }
+        default -> {
+            final var src = this.getAccountOrThrow(srcAccount);
+            final var dest = this.getAccountOrThrow(destAccount);
+            src.withdraw(value);
+            try {
+                dest.deposit(value);
+            } catch (final RuntimeException e) {
+                src.deposit(value); // should never throw.
+                throw e;
+            }
+        }
+        }
+    }
+
+    public int totalBalance() {
+        return switch (this.version) {
+        case EXERCISE_2 -> {
+            this.lock.lock();
+            try {
+                yield Arrays
+                    .stream(this.accounts)
+                    .mapToInt(Account::balance)
+                    .sum();
+            } finally {
+                this.lock.unlock();
+            }
+        }
+        default ->
+            Arrays.stream(this.accounts).mapToInt(Account::balance).sum();
+        };
+    }
+
+    public IntStream getRandomAccounts(final int limit) {
+        return this.rng.ints(0, this.accounts.length).distinct().limit(limit);
+    }
+
+    private final class Account {
+        private int balance;
+        private final ReentrantLock lock;
+
+        private Account(final int balance, final ReentrantLock lock) {
+            this.balance = balance;
+            this.lock = lock;
+        }
+
+        public int balance() {
+            return this.balance;
+        }
+
+        private void deposit(final int value) {
+            final var work = (Runnable) () -> {
+                if (this.balance > Integer.MAX_VALUE - value) {
+                    throw new RuntimeException(
+                        "value exceeds maximum account balance"
+                    );
+                }
+                this.balance += value;
+            };
+            if (Bank.this.version == Version.EXERCISE_3) {
+                this.lock.lock();
+                try {
+                    work.run();
+                } finally {
+                    this.lock.unlock();
+                }
+            } else {
+                work.run();
+            }
+        }
+
+        private void withdraw(final int value) {
+            final var work = (Runnable) () -> {
+                if (value > this.balance) {
+                    throw new RuntimeException("value exceeds account balance");
+                }
+                this.balance -= value;
+            };
+            if (Bank.this.version == Version.EXERCISE_3) {
+                this.lock.lock();
+                try {
+                    work.run();
+                } finally {
+                    this.lock.unlock();
+                }
+            } else {
+                work.run();
+            }
+        }
+    }
+}
---
+++ scripts/script_02/extern/Test.java
@@ -0,0 +1,93 @@
+package extern;
+
+import extern.Bank.Version;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+
+import static java.lang.System.out;
+
+public final class Test {
+    private static void test(final Bank.Version version) {
+        final var N_THREADS = 2;
+        final var N_ACCOUNTS = 10;
+        final var DEPOSIT_VALUE = 1000;
+
+        final var bank = new Bank.Builder()
+            .withVersion(version)
+            .setUpAccounts(10)
+            .build();
+
+        for (var i = 0; i < N_ACCOUNTS; ++i) {
+            bank.deposit(i, DEPOSIT_VALUE);
+        }
+
+        out.println(
+            "total balance before transfers: %d".formatted(bank.totalBalance())
+        );
+
+        final var pool = new ArrayList<Thread>();
+        pool.ensureCapacity(N_THREADS);
+
+        for (var i = 0; i < N_THREADS; ++i) {
+            final var thread = new Thread(() -> {
+                final var N_TRANSFERS = 100000;
+                final var TRANSFER_VALUE = 1;
+                final var transactionEnds = bank.getRandomAccounts(2).toArray();
+                for (var m = 0; m < N_TRANSFERS; ++m) {
+                    bank.transfer(
+                        transactionEnds[0],
+                        transactionEnds[1],
+                        TRANSFER_VALUE
+                    );
+                }
+            });
+            thread.start();
+            pool.add(thread);
+        }
+
+        for (final var thread : pool) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+
+        out.println(
+            "total balance after transfers: %d".formatted(bank.totalBalance())
+        );
+    }
+
+    public static void main(final String... args) {
+        out.println("Lockless implementation results:");
+        final var t1Start = Instant.now();
+        test(Version.ORIGINAL);
+        final var t1End = Instant.now();
+
+        out.println("\nBank-wide lock implementation results:");
+        final var t2Start = Instant.now();
+        test(Version.EXERCISE_2);
+        final var t2End = Instant.now();
+
+        out.println("\nAccount-wide lock implementation results");
+        final var t3Start = Instant.now();
+        test(Version.EXERCISE_3);
+        final var t3End = Instant.now();
+
+        final var t1Elapsed = Duration.between(t1Start, t1End).toMillis();
+        final var t2Elapsed = Duration.between(t2Start, t2End).toMillis();
+        final var t3Elapsed = Duration.between(t3Start, t3End).toMillis();
+
+        out.println("""
+
+            Lockless implementation elapsed time:          %dms
+            Bank-wide lock implementation elapsed time:    %dms
+            Account-wide lock implementation elapsed time: %dms"""
+            .formatted(t1Elapsed, t2Elapsed, t3Elapsed)
+        );
+
+    }
+}
