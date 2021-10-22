e10ca5e57baf93e0ede7f2fded5f9d66ee708eb9
oracle <oracle-main@pm.me>
2021-10-22 15:33:58 +0100

    Exercise 1.

--- scripts/script_03/extern/Bank.java
+++ scripts/script_03/extern/Bank.java
@@ -1,14 +1,17 @@
 package extern;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.IntStream;
+import java.util.concurrent.locks.ReentrantLock;
 
 class Bank {
     private final Map<Integer, Account> accounts;
+    private ReentrantLock lock;
 
     private Bank() {
         this.accounts = new HashMap<>();
+        this.lock = new ReentrantLock();
     }
 
     private Account getAccountOrThrow(final int accountId) {
@@ -19,27 +22,78 @@ class Bank {
         return account;
     }
 
-    public int createAccount() {
-        final var account = new Account();
-        final var accountId = this.accounts.size();
-        this.accounts.put(accountId, account);
-        return accountId;
+    public int registerAccount() {
+        this.lock.lock();
+        try {
+            final var accountId = this.accounts.size();
+            this.accounts.put(accountId, new Account());
+            return accountId;
+        } finally {
+            this.lock.unlock();
+        }
     }
 
     public void closeAccount(final int accountId) {
-        this.accounts.remove(accountId);
+        this.lock.lock();
+        try {
+            this.accounts.remove(accountId);
+        } finally {
+            this.lock.unlock();
+        }
     }
 
     public int balance(final int accountId) {
-        return this.getAccountOrThrow(accountId).balance();
+        this.lock.lock();
+        Account account;
+        try {
+            account = this.getAccountOrThrow(accountId);
+        } catch (RuntimeException e) {
+            this.lock.unlock();
+            throw e;
+        }
+        account.lock.lock();
+        this.lock.unlock();
+        try {
+            return account.balance();
+        } finally {
+            this.lock.unlock();
+        }
     }
 
     public void deposit(final int accountId, final int value) {
-        this.getAccountOrThrow(accountId).deposit(value);
+        this.lock.lock();
+        Account account;
+        try {
+            account = this.getAccountOrThrow(accountId);
+        } catch (RuntimeException e) {
+            this.lock.unlock();
+            throw e;
+        }
+        account.lock.lock();
+        this.lock.unlock();
+        try {
+            account.deposit(value);
+        } finally {
+            this.lock.unlock();
+        }
     }
 
     public void withdraw(final int accountId, final int value) {
-        this.getAccountOrThrow(accountId).withdraw(value);
+        this.lock.lock();
+        Account account;
+        try {
+            account = this.getAccountOrThrow(accountId);
+        } catch (RuntimeException e) {
+            this.lock.unlock();
+            throw e;
+        }
+        account.lock.lock();
+        this.lock.unlock();
+        try {
+            account.withdraw(value);
+        } finally {
+            this.lock.unlock();
+        }
     }
 
     public void transfer(
@@ -47,31 +101,86 @@ class Bank {
         final int destAccountId,
         final int value
     ) {
-        final var srcAccount = this.getAccountOrThrow(srcAccountId);
-        srcAccount.withdraw(value);
+        this.lock.lock();
+        Account srcAccount;
+        Account destAccount;
+
         try {
-            this.getAccountOrThrow(destAccountId).deposit(value);
+            srcAccount = this.getAccountOrThrow(srcAccountId);
+            destAccount = this.getAccountOrThrow(destAccountId);
+        } catch (final RuntimeException e) {
+            this.lock.unlock();
+            throw e;
+        }
+
+        srcAccount.lock.lock();
+        destAccount.lock.lock();
+        this.lock.unlock();
+
+        try {
+            srcAccount.withdraw(value);
+        } catch (final RuntimeException e) {
+            srcAccount.lock.unlock();
+            destAccount.lock.unlock();
+            throw e;
+        }
+
+        try {
+            destAccount.deposit(value);
         } catch (final RuntimeException e) {
             srcAccount.deposit(value);
+        } finally {
+            srcAccount.lock.unlock();
+            destAccount.lock.unlock();
         }
     }
 
     public int totalBalance() {
-        return this.accounts
-            .values()
-            .stream()
-            .mapToInt(Account::balance)
-            .sum();
+        this.lock.lock();
+        final var accounts = this.accounts.values();
+        for (final var account : accounts) {
+            account.lock.lock();
+        }
+        this.lock.unlock();
+        var sum = 0;
+        for (final var account : accounts) {
+            sum += account.balance();
+            account.lock.unlock();
+        }
+        return sum;
     }
 
-    public int totalBalanceOf(final IntStream accountIds) {
-        return accountIds
-            .map(id -> this.accounts.get(id).balance())
-            .sum();
+    public int totalBalanceOf(final int... accountIds) {
+        this.lock.lock();
+        final var accounts = new ArrayList<Account>();
+        accounts.ensureCapacity(accountIds.length);
+        for (final var accountId : accountIds) {
+            Account account;
+            try {
+                account = this.getAccountOrThrow(accountId);
+            } catch (final RuntimeException e) {
+                // We need to unlock every valid used up until this point before
+                // we propagate the error.
+                for (final var lockedAccount : accounts) {
+                    lockedAccount.lock.unlock();
+                }
+                throw e;
+            }
+            accounts.add(account);
+            account.lock.lock();
+        }
+        this.lock.unlock();
+        var sum = 0;
+        for (final var account : accounts) {
+            sum += account.balance();
+            account.lock.unlock();
+        }
+        return sum;
     }
 
     private static class Account {
         private int balance;
+        private ReentrantLock lock;
 
         private Account() {
             this.balance = 0;
@@ -82,6 +191,9 @@ class Bank {
         }
 
         private void deposit(final int value) {
+            if (value < 0) {
+                throw new RuntimeException("cannot deposit negative values");
+            }
             if (this.balance > Integer.MAX_VALUE - value) {
                 throw new RuntimeException(
                     "value exceeds maximum account balance"
@@ -91,8 +203,14 @@ class Bank {
         }
 
         private void withdraw(final int value) {
-            if (value > this.balance) {
-                throw new RuntimeException("value exceeds account balance");
+            if (value < 0) {
+                throw new RuntimeException("cannot withdraw negative values");
+            }
+            if (this.balance < Integer.MIN_VALUE + value) {
+                // balance integer underflow.
+                throw new RuntimeException(
+                    "value exceeds minimum account balance"
+                );
             }
             this.balance -= value;
         }
